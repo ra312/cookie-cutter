6473de4fd48d945fbc94ff581b10fb8afd99cab5
ra312 <akylzhanov.r@gmail.com>
2022-07-06 12:21:46 +0600

    Initialize add/existing-user-validation

---
+++ src/conduit/auth/views.py
@@ -0,0 +1,78 @@
+"""HTTP operations for auth."""
+
+from conduit.auth.models import User
+from mypy_extensions import TypedDict
+from passlib.hash import argon2
+from pyramid.httpexceptions import HTTPConflict
+from pyramid.httpexceptions import HTTPUnauthorized
+from pyramid.request import Request
+from pyramid.view import view_config
+
+# Python representation of openapi.yaml's UserResponse schema
+UserResponse = TypedDict("UserResponse", {"user": User})
+
+
+@view_config(
+    route_name="user",
+    renderer="json",
+    request_method="GET",
+    openapi=True,
+    permission="authenticated",
+)
+def current_user(request: Request) -> UserResponse:
+    """Get currently logged in user."""
+    return {"user": request.user}
+
+
+@view_config(
+    route_name="user",
+    renderer="json",
+    request_method="PUT",
+    openapi=True,
+    permission="authenticated",
+)
+def update(request: Request) -> UserResponse:
+    """Update currently logged in user."""
+    body = request.openapi_validated.body
+    user = request.user
+
+    for field in body.user.__dict__:
+        setattr(user, field, getattr(body.user, field))
+
+    return {"user": user}
+
+
+@view_config(route_name="users", renderer="json", request_method="POST", openapi=True)
+def register(request: Request) -> UserResponse:
+    """User registers to Conduit app."""
+    body = request.openapi_validated.body
+
+    if User.by_email(body.user.email, db=request.db):
+        raise HTTPConflict(f"Email {body.user.email} already exists.")
+
+    if User.by_username(body.user.username, db=request.db):
+        raise HTTPConflict(f"Username {body.user.username} already exists.")
+
+    user = User(
+        email=body.user.email,
+        username=body.user.username,
+        password_hash=argon2.hash(body.user.password),
+    )
+    request.db.add(user)
+    request.db.flush()  # so that user.id is set and JWT token can be generated
+    request.response.status_code = 201
+    return {"user": user}
+
+
+@view_config(
+    route_name="users.login", renderer="json", request_method="POST", openapi=True
+)
+def login(request: Request) -> UserResponse:
+    """User logs in."""
+    body = request.openapi_validated.body
+
+    user = User.by_email(body.user.email, db=request.db)
+    if user and user.verify_password(body.user.password):
+        return {"user": user}
+
+    raise HTTPUnauthorized()
