1ac7b7b9bb1bf5ecb964bd48ff200d95e500eddd
ra312 <akylzhanov.r@gmail.com>
2022-07-06 12:22:14 +0600

    Initialize fix/poetry-update

---
+++ .github/workflows/update.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -xeu
+
+# Fetch unshallow
+git fetch --unshallow
+
+# Update the ./nix/sources.json with the latest version of the branch
+niv update
+
+# Update Python packages
+poetry update --lock
+
+# Only continue if there are any changes
+if ! git diff-index --quiet HEAD; then
+  today=$(date -I)
+  # Commit the new changes and push them to the repository so a PR can be opened
+  git checkout -b "package-update-$today"
+  git config user.name "Package updater"
+  git commit -a -m "Package update $today"
+  git push -u origin "package-update-$today"
+  gh auth login --with-token <<< $GITHUB_TOKEN
+  gh pr create -B master -H "package-update-$today" -t "Update packages" -b ""
+fi
